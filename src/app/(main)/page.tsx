803c0423d67f95ce6d1e8452376f41da10a41852
YOON-CC <whdbscks77@gmail.com>
2024-07-22 17:03:09 +0900

    chore: 메인 디렉토리, 로그인 디렉토리 구분 (#1)

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import React from "react";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <img
         src="/icons/PwaLogo.png"
         style={{
---
+++ src/app/login/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Page() {
+  return <main>로그인</main>;
+}
