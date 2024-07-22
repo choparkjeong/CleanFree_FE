e8f3be303f057a251987e0cf07afb449a30f2244
YOON-CC <whdbscks77@gmail.com>
2024-07-22 16:18:50 +0900

    build: pwa 적용

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development", // workbox 안나오게
+});
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: ["styles"],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
+
+export default withPWA({
+  nextConfig,
+});
