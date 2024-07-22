c37a1583414153458d159d6f29818c1f1f9555d5
YOON-CC <whdbscks77@gmail.com>
2024-07-22 16:44:46 +0900

    feat: 스플레쉬 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,18 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>test2</main>;
+  return (
+    <main className={styles.main}>
+      <img
+        src="/icons/PwaLogo.png"
+        style={{
+          width: "300px",
+          height: "300px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      />
+    </main>
+  );
 }
