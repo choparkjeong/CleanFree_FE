03ba632167ea1bd45b43845489b303cbe1f8cceb
YOON-CC <whdbscks77@gmail.com>
2024-07-22 15:58:58 +0900

    테스트2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,5 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>test</main>;
+  return <main className={styles.main}>test2</main>;
 }
