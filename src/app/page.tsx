29fc710832859384d4b99cca35fc61b8ea3801db
YOON-CC <whdbscks77@gmail.com>
2024-07-23 17:04:05 +0900

    fix: 스플래시 깜빡임 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,29 +24,28 @@ export default function Home() {
     }
   }, []);
 
+  if (showSplash) {
+    return (
+      <div className={styles["splashScreen"]}>
+        <img className={styles["splashContent"]} src="/icons/MainLogo.png" />
+      </div>
+    );
+  }
+
   return (
     <>
-      {showSplash && (
-        <div className={styles["splashScreen"]}>
-          <img className={styles["splashContent"]} src="/icons/MainLogo.png" />
-        </div>
-      )}
-      {!showSplash && (
-        <>
-          <Header />
-          {/* 배너 저작권 표시
-          https://kr.freepik.com/free-vector/desk-calendar-with-marked-dates-3d-cartoon-style-icon-planning-time-and-meeting-scheduling-flat-vector-illustration-appointment-deadline-agenda-reminder-time-management-concept_29119114.htm#page=2&query=%EC%A2%85%203d&position=39&from_view=keyword&track=ais_user&uuid=32c1f6c5-459d-453e-b083-07ad8a0e3f05"작가 pch.vector출처 Freepik */}
-          <img
-            src="/banner/MainBanner.png"
-            className={styles["main-banner-img"]}
-          ></img>
-          <main className={styles["main-container"]}>
-            <CalendarComponent />
-            <CalendarStatus />
-          </main>
-          <NavBar />
-        </>
-      )}
+      <Header />
+      {/* 배너 저작권 표시
+      https://kr.freepik.com/free-vector/desk-calendar-with-marked-dates-3d-cartoon-style-icon-planning-time-and-meeting-scheduling-flat-vector-illustration-appointment-deadline-agenda-reminder-time-management-concept_29119114.htm#page=2&query=%EC%A2%85%203d&position=39&from_view=keyword&track=ais_user&uuid=32c1f6c5-459d-453e-b083-07ad8a0e3f05"작가 pch.vector출처 Freepik */}
+      <img
+        src="/banner/MainBanner.png"
+        className={styles["main-banner-img"]}
+      ></img>
+      <main className={styles["main-container"]}>
+        <CalendarComponent />
+        <CalendarStatus />
+      </main>
+      <NavBar />
     </>
   );
 }
