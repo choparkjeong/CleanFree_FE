ed56bf380b3417010fd54ca124e04194e3255522
YOON-CC <whdbscks77@gmail.com>
2024-07-23 16:56:41 +0900

    feat: 스플래시 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,52 @@
+"use client";
+
 import CalendarComponent from "@/components/ui/CalendarComponent";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "@/styles/pages/main.module.scss";
 import Header from "@/components/layout/Header";
-// import Footer from "@/components/layout/main/Footer";
 import NavBar from "@/components/layout/NavBar";
 import CalendarStatus from "@/components/ui/CalendarStatus";
-// import MainSlider from "@/components/layout/main/MainSlider";
-// import CircleAnimation from "@/components/ui/CircleAnimation";
 
 export default function Home() {
+  const [showSplash, setShowSplash] = useState(false);
+
+  useEffect(() => {
+    const hasSeenSplash = sessionStorage.getItem("hasSeenSplash");
+
+    if (!hasSeenSplash) {
+      setShowSplash(true);
+      const timer = setTimeout(() => {
+        setShowSplash(false);
+        sessionStorage.setItem("hasSeenSplash", "true");
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, []);
+
   return (
     <>
-      <Header />
-      {/* 배너 저작권 표시
-      https://kr.freepik.com/free-vector/desk-calendar-with-marked-dates-3d-cartoon-style-icon-planning-time-and-meeting-scheduling-flat-vector-illustration-appointment-deadline-agenda-reminder-time-management-concept_29119114.htm#page=2&query=%EC%A2%85%203d&position=39&from_view=keyword&track=ais_user&uuid=32c1f6c5-459d-453e-b083-07ad8a0e3f05"작가 pch.vector출처 Freepik */}
-      <img
-        src="/banner/MainBanner.png"
-        className={styles["main-banner-img"]}
-      ></img>
-      <main className={styles["main-container"]}>
-        <CalendarComponent />
-        <CalendarStatus />
-      </main>
-      <NavBar />
+      {showSplash && (
+        <div className={styles["splashScreen"]}>
+          <img className={styles["splashContent"]} src="/icons/MainLogo.png" />
+        </div>
+      )}
+      {!showSplash && (
+        <>
+          <Header />
+          {/* 배너 저작권 표시
+          https://kr.freepik.com/free-vector/desk-calendar-with-marked-dates-3d-cartoon-style-icon-planning-time-and-meeting-scheduling-flat-vector-illustration-appointment-deadline-agenda-reminder-time-management-concept_29119114.htm#page=2&query=%EC%A2%85%203d&position=39&from_view=keyword&track=ais_user&uuid=32c1f6c5-459d-453e-b083-07ad8a0e3f05"작가 pch.vector출처 Freepik */}
+          <img
+            src="/banner/MainBanner.png"
+            className={styles["main-banner-img"]}
+          ></img>
+          <main className={styles["main-container"]}>
+            <CalendarComponent />
+            <CalendarStatus />
+          </main>
+          <NavBar />
+        </>
+      )}
     </>
   );
 }
