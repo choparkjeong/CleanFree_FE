02a6cccdd2af45109265e4e5aab666643019c90d
YOON-CC <whdbscks77@gmail.com>
2024-07-23 17:07:39 +0900

    fix: 화면 분기처리로 스플래시 먼저 실행

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,20 +32,22 @@ export default function Home() {
     );
   }
 
-  return (
-    <>
-      <Header />
-      {/* 배너 저작권 표시
+  if (!showSplash) {
+    return (
+      <>
+        <Header />
+        {/* 배너 저작권 표시
       https://kr.freepik.com/free-vector/desk-calendar-with-marked-dates-3d-cartoon-style-icon-planning-time-and-meeting-scheduling-flat-vector-illustration-appointment-deadline-agenda-reminder-time-management-concept_29119114.htm#page=2&query=%EC%A2%85%203d&position=39&from_view=keyword&track=ais_user&uuid=32c1f6c5-459d-453e-b083-07ad8a0e3f05"작가 pch.vector출처 Freepik */}
-      <img
-        src="/banner/MainBanner.png"
-        className={styles["main-banner-img"]}
-      ></img>
-      <main className={styles["main-container"]}>
-        <CalendarComponent />
-        <CalendarStatus />
-      </main>
-      <NavBar />
-    </>
-  );
+        <img
+          src="/banner/MainBanner.png"
+          className={styles["main-banner-img"]}
+        ></img>
+        <main className={styles["main-container"]}>
+          <CalendarComponent />
+          <CalendarStatus />
+        </main>
+        <NavBar />
+      </>
+    );
+  }
 }
