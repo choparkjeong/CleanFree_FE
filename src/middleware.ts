d91d3a2133c4b6931a03be4ea8475eafe47b50a1
YOON-CC <whdbscks77@gmail.com>
2024-08-01 20:34:37 +0900

    style: 미들웨어 주석처리

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,17 +7,19 @@ export async function middleware(request: NextRequest) {
   const authorization = cookies().get("authorization")?.value;
 
   // 엑세스 토큰이랑, uuid가 널일경우
-  if (authorization === null) {
-    return NextResponse.redirect("https://www.cleanfree.site/login");
-  }
+  // if (authorization === null) {
+  //   return NextResponse.redirect("https://www.cleanfree.site/login");
+  // }
 
-  // 로그인을 안했을때 만 해당
-  if (!session) {
-    return NextResponse.redirect("https://www.cleanfree.site/login");
-  }
+  // // 로그인을 안했을때 만 해당
+  // if (!session) {
+  //   return NextResponse.redirect("https://www.cleanfree.site/login");
+  // }
 }
 
 export const config = {
   // 적용이 되는 곳 선정
-  matcher: ["/edit/:path*", "/write/:path*", "/i/flow/detail/:path*", "/"],
+  matcher: [],
+
+  // matcher: ["/edit/:path*", "/write/:path*", "/i/flow/detail/:path*", "/"],
 };
