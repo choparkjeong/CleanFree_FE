2fd853f0c964081e1b5b0f740711e32e54dce6f8
YOON-CC <whdbscks77@gmail.com>
2024-08-01 10:29:32 +0900

    fix: 모달 props 선언 제거

--- src/app/(userAccess)/layout.tsx
+++ src/app/(userAccess)/layout.tsx
@@ -1,11 +1,10 @@
 import { ReactNode } from "react";
 
-type Props = { children: ReactNode; modal: ReactNode };
-export default function Layout({ children, modal }: Props) {
+type Props = { children: ReactNode; };
+export default function Layout({ children }: Props) {
   return (
     <div>
       {children}
-      {modal}
     </div>
   );
 }
