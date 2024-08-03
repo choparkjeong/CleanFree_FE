308c9813d521cad7e702992269a1727ea2226cb6
YOON-CC <whdbscks77@gmail.com>
2024-08-03 21:13:30 +0900

    fix: 로딩 화면 제거

--- src/app/(userAccess)/result/[id]/page.tsx
+++ src/app/(userAccess)/result/[id]/page.tsx
@@ -43,7 +43,6 @@ interface ResultData {
 const Page: React.FC = (props) => {
   const pathName: any = props;
   const [resultData, setResultData] = useState<ResultData | null>(null);
-  const [loading, setLoading] = useState(true);
 
   const containerRef = useRef<HTMLDivElement | null>(null);
   const [activeIndex, setActiveIndex] = useState<number>(0);
@@ -53,7 +52,6 @@ const Page: React.FC = (props) => {
     const handleListData = async () => {
       const data = await getResultData(pathName.params.id);
       setResultData(data);
-      setLoading(false);
     };
 
     handleListData();
@@ -100,136 +98,115 @@ const Page: React.FC = (props) => {
 
   return (
     <>
-      {loading ? (
-        <div className={styles["loading"]}>LOADING...</div>
-      ) : (
-        <>
-          <ResultHeader />
-          <Title title="검색 내용" />
-          <div className={styles["result-container1"]}>
-            {resultData?.question}
+      <ResultHeader />
+      <Title title="검색 내용" />
+      <div className={styles["result-container1"]}>{resultData?.question}</div>
+      <Title title="화장품 추천" />
+      <div className={styles["result-container2"]}>
+        <div className={styles["details"]}>
+          <div className={styles["details-layout1"]}>
+            <img
+              src={resultData?.cosmetics[activeIndex].image}
+              style={{ width: "100%", height: "100%", borderRadius: "5px" }}
+            />
           </div>
-          <Title title="화장품 추천" />
-          <div className={styles["result-container2"]}>
-            <div className={styles["details"]}>
-              <div className={styles["details-layout1"]}>
-                <img
-                  src={resultData?.cosmetics[activeIndex].image}
-                  alt={"이미지"}
-                  style={{ width: "100%", height: "100%", borderRadius: "5px" }}
-                />
+          <div className={styles["details-layout2"]}>
+            <div className={styles["details-layout2-element1"]}>
+              {resultData?.cosmetics[activeIndex]?.cosmetic || "Loading..."}
+            </div>
+            <div className={styles["details-layout2-element2"]}>
+              ⓘ 가격 :{" "}
+              {resultData?.cosmetics[activeIndex]?.costRange || "Loading..."}
+            </div>
+            <div className={styles["details-layout2-element3"]}>
+              <div className={styles["review1"]}>
+                {"★".repeat(resultData?.cosmetics[activeIndex].maxReview.score)}
+
+                <div>
+                  {resultData?.cosmetics[activeIndex].maxReview.summary}
+                </div>
               </div>
-              <div className={styles["details-layout2"]}>
-                <div className={styles["details-layout2-element1"]}>
-                  {resultData?.cosmetics[activeIndex]?.cosmetic || "Loading..."}
-                </div>
-                <div className={styles["details-layout2-element2"]}>
-                  ⓘ 가격 :{" "}
-                  {resultData?.cosmetics[activeIndex]?.costRange ||
-                    "Loading..."}
-                </div>
-                <div className={styles["details-layout2-element3"]}>
-                  <div className={styles["review1"]}>
-                    {"★".repeat(
-                      resultData?.cosmetics[activeIndex].maxReview.score
-                    )}
+              <div className={styles["review2"]}>
+                {"★".repeat(resultData?.cosmetics[activeIndex].minReview.score)}
 
-                    <div>
-                      {resultData?.cosmetics[activeIndex].maxReview.summary}
-                    </div>
-                  </div>
-                  <div className={styles["review2"]}>
-                    {"★".repeat(
-                      resultData?.cosmetics[activeIndex].minReview.score
-                    )}
-
-                    <div>
-                      {resultData?.cosmetics[activeIndex].minReview.summary}
-                    </div>
-                  </div>
+                <div>
+                  {resultData?.cosmetics[activeIndex].minReview.summary}
                 </div>
               </div>
             </div>
-
-            <div className={styles["scroll-container"]} ref={containerRef}>
-              <div className={styles["scroll-content"]}>
-                <div className={styles["scroll-item-dummy"]}></div>
-
-                {resultData?.cosmetics?.map((product, index) => (
-                  <div
-                    key={product.cosmetic + index}
-                    className={`${styles["scroll-item"]} ${
-                      activeIndex === index ? styles["active"] : ""
-                    }`}
-                  >
-                    <img
-                      src={product.image}
-                      alt="이미지 없음"
-                      style={{
-                        width: "55px",
-                        height: "55px",
-                        borderRadius: "5px",
-                      }}
-                    />
-                    <div>{product.cosmetic}</div>
-                  </div>
-                ))}
-                <div className={styles["scroll-item-dummy"]}></div>
-              </div>
-            </div>
           </div>
-          <Title title="성분 분석" />
-          <div className={styles["result-container3"]}>
-            {resultData?.ingredients.map((ingredient, index) => (
-              <div key={index}>{ingredient}</div>
-            ))}
-          </div>
-          <Title title="출처 및 참고" />
-          <div className={styles["result-container4"]}>
-            {resultData?.references.youtube.map((url, index) => (
+        </div>
+
+        <div className={styles["scroll-container"]} ref={containerRef}>
+          <div className={styles["scroll-content"]}>
+            <div className={styles["scroll-item-dummy"]}></div>
+
+            {resultData?.cosmetics?.map((product, index) => (
               <div
-                key={`youtube-${index}`}
-                className={styles["reference-item1"]}
+                key={product.cosmetic + index}
+                className={`${styles["scroll-item"]} ${
+                  activeIndex === index ? styles["active"] : ""
+                }`}
               >
-                <Link
-                  href={url}
-                  className={styles["link1"]}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaYoutube /> 바로가기
-                </Link>
+                <img
+                  src={product.image}
+                  alt="이미지 없음"
+                  style={{ width: "55px", height: "55px", borderRadius: "5px" }}
+                />
+                <div>{product.cosmetic}</div>
               </div>
             ))}
-            {resultData?.references.blog.map((url, index) => (
-              <div key={`blog-${index}`} className={styles["reference-item2"]}>
-                <Link
-                  href={url}
-                  className={styles["link2"]}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaBlogger /> 바로가기
-                </Link>
-              </div>
-            ))}
-            {resultData?.references.etc.map((url, index) => (
-              <div key={`etc-${index}`} className={styles["reference-item3"]}>
-                <Link
-                  href={url}
-                  className={styles["link3"]}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaLink /> 바로가기
-                </Link>
-              </div>
-            ))}
+            <div className={styles["scroll-item-dummy"]}></div>
           </div>
-          <ResultFooter />
-          <ResultNav />
-        </>
-      )}
+        </div>
+      </div>
+      <Title title="성분 분석" />
+      <div className={styles["result-container3"]}>
+        {resultData?.ingredients.map((ingredient, index) => (
+          <div key={index}>{ingredient}</div>
+        ))}
+      </div>
+      <Title title="출처 및 참고" />
+      <div className={styles["result-container4"]}>
+        {resultData?.references.youtube.map((url, index) => (
+          <div key={`youtube-${index}`} className={styles["reference-item1"]}>
+            <Link
+              href={url}
+              className={styles["link1"]}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaYoutube /> 바로가기
+            </Link>
+          </div>
+        ))}
+        {resultData?.references.blog.map((url, index) => (
+          <div key={`blog-${index}`} className={styles["reference-item2"]}>
+            <Link
+              href={url}
+              className={styles["link2"]}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaBlogger /> 바로가기
+            </Link>
+          </div>
+        ))}
+        {resultData?.references.etc.map((url, index) => (
+          <div key={`etc-${index}`} className={styles["reference-item3"]}>
+            <Link
+              href={url}
+              className={styles["link3"]}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLink /> 바로가기
+            </Link>
+          </div>
+        ))}
+      </div>
+      <ResultFooter />
+      <ResultNav />
     </>
   );
 };
